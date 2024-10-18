8c84a9f532a2c2653157c4ab0e08338c2ce6aa4a
bhushal11 <53200831+bhushal11@users.noreply.github.com>
2024-10-18 16:31:48 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,11 +8,22 @@ note
 
 #echo "$(date '+%Y-%m-%d_%H-%M-%S')"
 
+function show_date {
+	echo "Today is: $(date '+%Y-%m-%d_%H-%M-%S')i"
+}
+function create_backup {
+
 timestamp=$(date '+%Y-%m-%d_%H-%M-%S')
 
-backup_dir="${timestamp}_backup.zip"
+backup_dir="/home/ubuntu/backups/${timestamp}_backup.zip"
 
 zip -r $backup_dir $1 
 
 echo "backup completed"
+
+}
+
+show_date
+create_backup $1
+
  
